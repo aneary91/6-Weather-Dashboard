37e7ff808b7d185caa7c079bcd9929180abb8c3d
AllisonWonderland <aneary91@gmail.com>
2021-04-14 21:31:40 -0500

    sending changes

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  var apiKey = "285b694a07fb7ee5601483eddbb06432";
+
+  $("#goButton").on("click", function () {
+    // grabbing the search values of the city
+    var searchedCity = $("#yoCity").val();
+    // empties search input after search is intiated
+    $("#yoCity").val("");
+    console.log(searchedCity);
+    currentForecast(searchedCity);
+  });
+
+  function currentForecast(city) {
+    $.ajax({
+      type: "GET",
+      url:
+        "api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=" +
+        apiKey +
+        "&units=imperial",
+      dataType: "json",
+      success: function (response) {
+        console.log(response);
+      },
+    });
+  }
+});
