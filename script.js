f4a2e03c36cd8458eb47ce9ace9471ca267df316
AllisonWonderland <aneary91@gmail.com>
2021-06-15 11:36:45 -0500

    redoing script so I have a better understanding

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-$(document).ready(function () {
-  var apiKey = "285b694a07fb7ee5601483eddbb06432";
+var apiKey = "285b694a07fb7ee5601483eddbb06432";
 
-  $("#goButton").on("click", function () {
-    // grabbing the search values of the city
-    var searchedCity = $("#yoCity").val();
-    // empties search input after search is intiated
-    $("#yoCity").val("");
-    console.log(searchedCity);
-    currentForecast(searchedCity);
-  });
-
-  function currentForecast(city) {
-    $.ajax({
-      type: "GET",
-      url:
-        "http://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&appid=" +
-        apiKey +
-        "&units=imperial",
-      dataType: "json",
-      success: function (response) {
-        const yoCityTitle = $('<h3>').addClass('card-title').text(response.name)
-          console.log(yoCityTitle)
-      },
-    });
+// we are going to grab the input from the input element on click
+document.getElementById('searchButton').onclick = function () {
+  var enteredCity = document.getElementById('cityInput').value
+  if (enertedCity) {
+    // this is to clear the input feail once selected
+    document.getElementById('cityInput').value = ''
+    seachCity(enertedCity)
   }
-});
+}
+
+// instructing searched citied appear underneath search bar as a list from hte local localStorage
+function makeList(){
+  var savedCity = JSON.parse(localStorage.getItem('cities')) || []
+  for (let i = 0; i < savedcities.length; i++) {
+    createList(savedcities[i])
+  }
+}
+makeList()
+
+// created a seach city function
+
+
